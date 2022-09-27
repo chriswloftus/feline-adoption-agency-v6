4ac1b9ec17d12222fbb92c7fa1d1686cfe75eada
cwl <cwl@aber.ac.uk>
2022-09-27 16:12:44 +0100

    Initial commit

---
+++ app/src/main/java/uk/ac/aber/dcs/cs31620/faa/model/CatList.kt
@@ -0,0 +1,131 @@
+/**
+ * Hard-coded list of cat data
+ * @author Chris Loftus
+ */
+package uk.ac.aber.dcs.cs31620.faa.model
+
+import uk.ac.aber.dcs.cs31620.faa.R
+import java.time.LocalDate
+
+val cats = listOf(
+    Cat(
+        "Tibs", Gender.MALE, "Short hair",
+        "Lorem ipsum dolor sit amet, ...",
+        R.drawable.cat1,
+        LocalDate.now()
+    ),
+    Cat(
+        "Tibs", Gender.MALE, "Short hair",
+        "Lorem ipsum dolor sit amet, ...",
+        R.drawable.cat1,
+        LocalDate.now()
+    ),
+    Cat(
+        "Tibs", Gender.MALE, "Short hair",
+        "Lorem ipsum dolor sit amet, ...",
+        R.drawable.cat1,
+        LocalDate.now()
+    ),
+    Cat(
+        "Tibs", Gender.MALE, "Short hair",
+        "Lorem ipsum dolor sit amet, ...",
+        R.drawable.cat1,
+        LocalDate.now()
+    ),
+    Cat(
+        "Tibs", Gender.MALE, "Short hair",
+        "Lorem ipsum dolor sit amet, ...",
+        R.drawable.cat1,
+        LocalDate.now()
+    ),
+    Cat(
+        "Tibs", Gender.MALE, "Short hair",
+        "Lorem ipsum dolor sit amet, ...",
+        R.drawable.cat1,
+        LocalDate.now()
+    ),
+    Cat(
+        "Tibs", Gender.MALE, "Short hair",
+        "Lorem ipsum dolor sit amet, ...",
+        R.drawable.cat1,
+        LocalDate.now()
+    ),
+    Cat(
+        "Tibs", Gender.MALE, "Short hair",
+        "Lorem ipsum dolor sit amet, ...",
+        R.drawable.cat1,
+        LocalDate.now()
+    ),
+    Cat(
+        "Tibs", Gender.MALE, "Short hair",
+        "Lorem ipsum dolor sit amet, ...",
+        R.drawable.cat1,
+        LocalDate.now()
+    ),
+    Cat(
+        "Tibs", Gender.MALE, "Short hair",
+        "Lorem ipsum dolor sit amet, ...",
+        R.drawable.cat1,
+        LocalDate.now()
+    ),
+    Cat(
+        "Tibs", Gender.MALE, "Short hair",
+        "Lorem ipsum dolor sit amet, ...",
+        R.drawable.cat1,
+        LocalDate.now()
+    ),
+    Cat(
+        "Tibs", Gender.MALE, "Short hair",
+        "Lorem ipsum dolor sit amet, ...",
+        R.drawable.cat1,
+        LocalDate.now()
+    ),
+    Cat(
+        "Tibs", Gender.MALE, "Short hair",
+        "Lorem ipsum dolor sit amet, ...",
+        R.drawable.cat1,
+        LocalDate.now()
+    ),
+    Cat(
+        "Tibs", Gender.MALE, "Short hair",
+        "Lorem ipsum dolor sit amet, ...",
+        R.drawable.cat1,
+        LocalDate.now()
+    ),
+    Cat(
+        "Tibs", Gender.MALE, "Short hair",
+        "Lorem ipsum dolor sit amet, ...",
+        R.drawable.cat1,
+        LocalDate.now()
+    ),
+    Cat(
+        "Tibs", Gender.MALE, "Short hair",
+        "Lorem ipsum dolor sit amet, ...",
+        R.drawable.cat1,
+        LocalDate.now()
+    ),
+    Cat(
+        "Tibs", Gender.MALE, "Short hair",
+        "Lorem ipsum dolor sit amet, ...",
+        R.drawable.cat1,
+        LocalDate.now()
+    ),
+    Cat(
+        "Tibs", Gender.MALE, "Short hair",
+        "Lorem ipsum dolor sit amet, ...",
+        R.drawable.cat1,
+        LocalDate.now()
+    ),
+    Cat(
+        "Tibs", Gender.MALE, "Short hair",
+        "Lorem ipsum dolor sit amet, ...",
+        R.drawable.cat1,
+        LocalDate.now()
+    ),
+    Cat(
+        "Tibs", Gender.MALE, "Short hair",
+        "Lorem ipsum dolor sit amet, ...",
+        R.drawable.cat1,
+        LocalDate.now()
+    )
+)
